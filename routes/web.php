110893c1af7446f56a18bdaff052a7f99534c2ce
MUTWIRI0922 <amutwiri07@gmail.com>
2026-08-06 19:55:04 +0300

    Dashboardcontroller routes edit

--- routes/web.php
+++ routes/web.php
@@ -80,8 +80,8 @@ Route::middleware(['landlord.auth'])->group(function () {
     })->name('dashboard.add-house');
     Route::get('/landlord/houses', [DashboardController::class, 'landlordHouses'])->name('landlord.houses');
     Route::post('/house-upload', [housedetailscontroller::class, 'upload'])->name('house.upload');
-    Route::get('/landlord/houses/{id}', [DashboardController::class, 'showHouseDetails'])->name('landlord.house.details');
-    Route::get('/landlord/houses/{id}/edit', [DashboardController::class, 'edit'])->name('dashboard.edit-house');
+    Route::get('/landlord/houses/{id}', [Dashboardcontroller::class, 'showHouseDetails'])->name('landlord.house.details');
+    Route::get('/landlord/houses/{id}/edit', [Dashboardcontroller::class, 'edit'])->name('dashboard.edit-house');
     Route::get('/landlord/profile', [RegistrationController::class, 'showProfile'])->name('landlord.profile');
     Route::get('/landlord/profile/edit', [RegistrationController::class, 'editProfile'])->name('landlord.editProfile');
     Route::post('/landlord/updateProfile', [RegistrationController::class, 'updateProfile'])->name('landlord.updateProfile');
@@ -89,8 +89,8 @@ Route::middleware(['landlord.auth'])->group(function () {
         return view('Landlord.passwordchange');
     })->name('landlord.passwordchange');
     Route::post('/landlord/change-password', [RegistrationController::class, 'changePassword'])->name('landlord.changePassword');
-    Route::put('/houses/{id}', [\App\Http\Controllers\DashboardController::class, 'houseupdate'])->name('house.update');
-    Route::delete('/houses/{id}', [\App\Http\Controllers\DashboardController::class, 'housedelete'])->name('house.delete');
+    Route::put('/houses/{id}', [\App\Http\Controllers\Dashboardcontroller::class, 'houseupdate'])->name('house.update');
+    Route::delete('/houses/{id}', [\App\Http\Controllers\Dashboardcontroller::class, 'housedelete'])->name('house.delete');
     Route::get('/landlord/support', function() {
         return view('Landlord.support');
     })->name('landlord.support');
